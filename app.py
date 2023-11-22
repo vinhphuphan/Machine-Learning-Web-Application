e7cb511f3c8153d8eac1e6593493672c3df24755
vinhphuphan <vinhphu.phan@students.mq.edu.au>
2023-11-22 16:13:15 +1100

    get app ready for deployment

--- app.py
+++ app.py
@@ -97,4 +97,4 @@ def handle_form_submission():
     return render_template("index.html")
 
 if __name__ == '__main__':
-    app.run(port=5500, debug=True)
+    app.run(host = "0.0.0.0", port=5500, debug=True)
